2624ea6ef4bbec27258e23f4a26a49dc722b6035
castaneai <castaneai@castaneai.net>
2022-04-22 20:07:28 +0900

    axum

--- src/packets.rs
+++ src/packets.rs
@@ -1,7 +1,4 @@
 use binrw::binrw;
-use tokio_util::codec::LengthDelimitedCodec;
-
-const MAX_PACKET_SIZE: usize = 4096;
 
 #[binrw]
 #[brw(little, magic = b"KAZAHANE 1.0.0")]
@@ -20,17 +17,6 @@ enum PacketType {
     HelloRequest = 0x01,
 }
 
-fn build_codec() -> LengthDelimitedCodec {
-    LengthDelimitedCodec::builder()
-        .little_endian()
-        .length_field_offset(15)
-        .length_field_type::<u16>()
-        .length_adjustment(15 + 2) // magic + len
-        .num_skip(0)
-        .max_frame_length(MAX_PACKET_SIZE)
-        .new_codec()
-}
-
 #[test]
 fn parse_kazahane_packet() {
     use binrw::io::Cursor;
@@ -40,37 +26,4 @@ fn parse_kazahane_packet() {
     let p = KazahanePacket::read(&mut data).unwrap();
     assert_eq!(PacketType::HelloRequest, p.packet_type);
     assert_eq!(b"hello".to_vec(), p.payload);
-}
-
-#[tokio::test]
-async fn packet_with_framed() {
-    use std::io::Cursor;
-    use tokio::{
-        io::AsyncWriteExt,
-        net::{TcpListener, TcpStream},
-    };
-    use tokio_stream::StreamExt;
-    use tokio_util::codec::FramedRead;
-    use binrw::BinRead;
-
-    let listener = TcpListener::bind("localhost:0").await.unwrap();
-    let addr = listener.local_addr().unwrap();
-    tokio::spawn(async move {
-        let mut client = TcpStream::connect(addr).await.unwrap();
-        client.write_all(b"KAZAHA").await.unwrap();
-        client
-            .write_all(b"NE 1.0.0\x01\x05\x00hello")
-            .await
-            .unwrap();
-    });
-    let (client, _) = listener.accept().await.unwrap();
-    let mut reader = FramedRead::new(client, build_codec());
-    let frame = reader.next().await;
-    assert_eq!(frame.is_some(), true);
-    let frame = frame.unwrap();
-    assert_eq!(frame.is_ok(), true);
-    let frame = frame.unwrap().freeze();
-    let mut input = Cursor::new(frame.as_ref());
-    let packet = KazahanePacket::read(&mut input).unwrap();
-    assert_eq!(packet.payload, b"hello");
 }
---
+++ tests/server_test.rs
@@ -0,0 +1,23 @@
+use axum::{
+    routing::{get}, Router,
+};
+use axum::body::Body;
+use axum::http::{Request, StatusCode};
+use tower::util::ServiceExt;
+
+fn test_app() -> Router {
+    Router::new()
+        .route("/", get(|| async { "Hello" }))
+}
+
+#[tokio::test]
+async fn hello_world() {
+    let app = test_app();
+
+    let resp = app.oneshot(
+        Request::builder().uri("/").body(Body::empty()).unwrap()
+    ).await.unwrap();
+    assert_eq!(resp.status(), StatusCode::OK);
+    let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+    assert_eq!(&body[..], b"Hello");
+}
